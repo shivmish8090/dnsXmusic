82b21ea9bdaac11b324c2e107ae8742a168ce612
pagal4206 <dilipjaat183@gmail.com>
2025-03-05 21:35:27 +0530

    Update __main__.py

--- DnsXMusic/__main__.py
+++ DnsXMusic/__main__.py
@@ -59,6 +59,8 @@ async def init():
     await idle()
 
 
+import asyncio
+
 if __name__ == "__main__":
-    asyncio.get_event_loop_policy().get_event_loop().run_until_complete(init())
+    asyncio.run(init())
     LOGGER("DnsXMusic").info("Stopping DnsXMusic! GoodBye")
