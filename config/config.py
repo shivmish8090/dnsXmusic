fdfaf602e188704fad7eaf820b502d560f048c29
shivmish8090 <apilania55@gmail.com>
2025-04-07 12:46:14 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -109,7 +109,7 @@ GIT_TOKEN = getenv(
 
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
-    "SUPPORT_CHANNEL", "https://t.me/Dns_Official_Channel"
+    "SUPPORT_CHANNEL", "https://t.me/SanatanVibe"
 )  # Example:- https://t.me/TheTeamVivek
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", "https://t.me/dns_support_group"
