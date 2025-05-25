08d2bfbc6cce9811541243cee393b3f0851ccde8
shivmish8090 <apilania55@gmail.com>
2025-05-25 19:16:08 +0530

    Update Youtube.py

--- DnsXMusic/platforms/Youtube.py
+++ DnsXMusic/platforms/Youtube.py
@@ -7,7 +7,7 @@ from typing import Union
 import yt_dlp
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
-from youtubesearchpython.future import VideosSearch
+from youtubesearchpython.__future__ import VideosSearch
 
 from DnsXMusic.utils.database import is_on_off
 from DnsXMusic.utils.formatters import time_to_seconds
